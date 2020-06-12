a94413fd7acfe27c651aee97eae4334e72539c61
Juanma Canahuate <juan.manuel.canahuare@gmail.com>
2020-06-12 15:15:55 -0400

    UI 2.0

--- pages/games/_id.vue
+++ pages/games/_id.vue
@@ -1,5 +1,5 @@
 <template>
-  <div>
+  <div class="bg-gray-600">
     <div class="hero bg-gray-600" :style="`background:url(${backgroundImage}); background-size:cover`">
       <div class="container mx-auto flex flex-col md:flex-row items-end pb-4" style="height:250px">
         <div class="w-full md:w-1/4"></div>
@@ -14,27 +14,27 @@
       <div class="w-full md:w-1/4 -mt-16">
         <img :src="games.background_image_additional" alt="cover" class="mb-8">
         <div class="mb-4">
-          <span class="font-semibold">Platforms:</span>
-          <span v-for="platform in games.platforms" :key="platform.platform.id">{{ platform.platform.name }}, </span>
+          <span class="font-semibold text-white">Platforms:</span>
+          <span v-for="platform in games.platforms" :key="platform.platform.id" class="text-white">{{ platform.platform.name }}, </span>
         </div>
 
         <div class="mb-4">
-          <span class="font-semibold">Released:</span>
-          <span>{{ games.released }}</span>
+          <span class="font-semibold text-white">Released:</span>
+          <span class="text-white">{{ games.released }}</span>
         </div>
 
         <div class="mb-6">
-          <a :href="getOfficialWebsite">Official Website</a>
+          <a :href="getOfficialWebsite" class="text-white">Official Website</a>
         </div>
 
-        <div class="mb-8">
-          <div class="text-5xl font-semibold">{{ games.rating }}%</div>
-          <div class="font-semibold">Overall Rating</div>
+        <div class="mb-6">
+          <div class="text-5xl font-semibold text-white">{{ games.rating }}%</div>
+          <div class="font-semibold text-white">Overall Rating</div>
         </div>
 
       </div>
       <div class="w-full md:w-3/4 md:ml-12 py-8 leading-normal">
-      <p class="mb-12">{{ games.description_raw }}</p>
+      <p class="mb-12 text-white">{{ games.description_raw }}</p>
       
       <!-- <div class="flex flex-wrap -mx-4">
         <a v-for="screenshot in game.screenshots" :key="screenshot.cloudinary_id" class="w-full md:w-1/4 px-4 mb-12 no-underline">
--- pages/index.vue
+++ pages/index.vue
@@ -1,12 +1,13 @@
 <template>
-  <div class="flex mb-4">
-    <div class="w-full md:w-1/4 bg-gray-400 p-4 text-center text-gray-700"> 
+  <div class="flex mb-4 h-screen">
+    <div class="w-full md:w-1/4 bg-gray-400 p-4 text-center text-gray-700 h-screen"> 
+      <input class="appearance-none bg-transparent border-none w-full text-white mr-3 py-1 px-2 leading-tight focus:outline-none" v-model="limite" id="limit" placeholder="Limite de resultados">
     <!--
       Esta es la barra de la izquierda, donde se va a agregar la busqueda de plataformas. ETC
     -->
   
     </div>
-    <div class="w-full md:w-3/4 bg-gray-500 p-4 text-center text-gray-200">
+    <div class="w-full md:w-3/4 bg-gray-500 p-4 text-center text-gray-200 h-screen">
       <!-- 
         Esta es la barra de la derecha, que tiene el contenido de las busquedas
         y tiene el contenido de los juegos.
@@ -15,13 +16,13 @@
       <input class="appearance-none bg-transparent border-none w-full text-white mr-3 py-1 px-2 leading-tight focus:outline-none" v-model="keyword" id="search" placeholder="Search">
       
       </div>
-      <div class="border-b border-2 border-gray-500 py-2 bg-gray-400 px-2 max-h-full">
+      <div class="border-b border-2 border-gray-500 py-2 bg-gray-400 px-2">
         <div>
           <h1 class="font-heading uppercase mb-8 text-black" v-if="!keyword">Popular Games</h1>  
           <h1 class="font-heading uppercase mb-8 text-black" v-if="keyword">Search game {{ keyword }}</h1>
         </div>  
-        <div class="game-container flex flex-wrap border-3 border-gray-500">
-          <nuxt-link :to="'/games/' + game.id" v-for="game in games" :key="game.id" class="w-full md:w-1/5 mb-5 no-underline border-2">
+        <div class="game-container flex flex-wrap border-3 border-gray-500 h-screen">
+          <nuxt-link :to="'/games/' + game.id" v-for="game in games" :key="game.id" class="w-full md:w-1/5">
             <img thumbnail fluid :src="game.background_image" alt="cover" class="md:h-20 w-full">
             <div class="text-black font-semibold text-base overflow-hidden whitespace-no-wrap overflow-dots">{{ game.name }}</div>
             <!-- <div class="text-black text-base overflow-hidden whitespace-no-wrap overflow-dots">{{ game.genres[0] }}</div> --> <!-- tengo que arreglar este error --> 
@@ -36,18 +37,31 @@
 import axios from 'axios'
 
 export default {
-  // Esto es para buscar el juego tecleado en la barra de buscar.
   data: () => ({
-    games: [],
-    keyword: ""
+    games: [], // aqui se guardaran los juegos en un array
+    keyword: "", // palabra que se buscara
+    limite: "" // limite de items
     }),
 
+    // Esto es para que cuando se cargue la pagina, automatico le lleguen datos.
+    async fetch(){
+      return axios(`https://rawg-video-games-database.p.rapidapi.com/games`)
+      .then(res => {
+        this.games= res.data.results;
+      })
+      .catch(err => {
+        console.error(err);
+      });
+    },
+    
+  // buscar juegos
   methods: {
     searchGames() {
       axios
         .get("https://rawg-video-games-database.p.rapidapi.com/games", {  // Se empleza con la llamada a la api
           params: {
-            search: this.keyword // Se busca la palabra deseada
+            search: this.keyword, // Se busca la palabra deseada
+            page_size: this.limite // limite de resultados
           }
         })
         .then(res => {
@@ -61,6 +75,9 @@ export default {
 
   watch: { // Esto es para que cuando cambie el valor de keyword, se busque automaticamente
     keyword(newKeyword, oldKeyword){
+      this.searchGames();
+    },
+    limite(newLimite, oldLimite){
       this.searchGames();
     }
   }
