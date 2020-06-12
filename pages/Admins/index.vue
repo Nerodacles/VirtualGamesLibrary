0ebec2db1a36799890c7466875fec22052add7db
josephGP2 <65329391+josephGP2@users.noreply.github.com>
2020-06-12 01:50:34 -0400

    update login
    ya se puede iniciar sesión

---
+++ pages/Admins/index.vue
@@ -0,0 +1,13 @@
+<template>
+        <div>
+          <h1 class="font-heading uppercase mb-8 text-black" v-if="!keyword">Funciona coño</h1>  
+        
+        </div>  
+</template>
+
+<script>
+export default{
+
+}
+
+</script>
--- pages/login/index.vue
+++ pages/login/index.vue
@@ -19,13 +19,15 @@
             }
         },
         methods: {
+            //Aqui es donde esta el wiri
             login() {
                 if(this.input.username != "" && this.input.password != "") {
-                    if(this.input.username == this.$parent.mockAccount.username && this.input.password == this.$parent.mockAccount.password) {
+                    if(this.input.username == 'admin' && this.input.password == 'admin') {
                         this.$emit("authenticated", true);
                         this.$router.replace({ name: "secure" });
+                         location.href="/Admins";
                     } else {
-                        console.log("The username and / or password is incorrect");
+                        alert("The username and / or password is incorrect");
                     }
                 } else {
                     console.log("A username and password must be present");
@@ -47,9 +49,10 @@
 </style>
 <template>
     <div id="login">
-        <h1>Login</h1>
+        <h1>Admins Login</h1>
         <input type="text" name="username" v-model="input.username" placeholder="Username" />
         <input type="password" name="password" v-model="input.password" placeholder="Password" />
         <button type="button" v-on:click="login()">Login</button>
     </div>
 </template>
+
