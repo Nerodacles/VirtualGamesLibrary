1f87014fa933fa13d5841083fd65466515434338
Juanma Canahuate <juan.manuel.canahuare@gmail.com>
2020-06-15 21:27:54 -0400

    Added reddit link and controling errors

--- pages/games/_id.vue
+++ pages/games/_id.vue
@@ -1,50 +1,73 @@
 <template>
-  <div class="">
+  <div class="text-white">
     <div class="hero bg-gray-600" :style="`background:url(${backgroundImage}); background-size:cover`">
       <div class="container mx-auto flex flex-col md:flex-row items-end pb-4" style="height:250px">
         <div class="w-full md:w-1/4"></div>
         <div class="w-full md:w-3/4 md:ml-12">
-          <h1 class="font-heading text-white">{{ games.name }}</h1>
-          <div class="text-white">{{ games.developers[0].name }}</div>
+          <h1 class="font-heading ">{{ games.name }}</h1>
+          <div v-if="(games.publishers != null) && (games.publishers == [])">{{ games.publishers[0].name }}</div>
         </div>
       </div>
     </div>
 
-     <div class="container mx-auto flex flex-col md:flex-row">
+     <div class="container mx-auto flex flex-col md:flex-row ">
       <div class="w-full md:w-1/4 -mt-16">
-        <img :src="games.background_image " alt="cover" class="mb-8">
-        <div class="mb-4">
-          <span class="font-semibold text-white">Platforms:</span>
-          <span v-for="platform in games.platforms" :key="platform.platform.id" class="text-white">{{ platform.platform.name }}, </span>
+        <img :src="games.background_image " alt="cover" class="mb-8" v-if="games.background_image != null">
+
+        <div class="mb-4" v-if="games.platforms != null">
+          <span class="font-semibold">Platforms:</span>
+          <span v-for="platform in games.platforms" :key="platform.platform.id" class="">{{ platform.platform.name }}, </span>
         </div>
 
-        <div class="mb-4">
-          <span class="font-semibold text-white">Released:</span>
-          <span class="text-white">{{ games.released }}</span>
+        <div class="mb-2" v-if="games.genres != null">
+          <span class="font-semibold">Genres:</span>
+          <span v-for="genres in games.genres" :key="genres.id" >{{ genres.name }}, </span>
+        </div>
+
+        <div class="mb-2" v-if="games.esrb_rating != null">
+          <span class="font-semibold">ESRB Rating:</span>
+          <span>{{ games.esrb_rating.name }}</span>
+        </div>
+
+        <div class="mb-4" v-if="games.released =! null">
+          <span class="font-semibold">Released:</span>
+          <span>{{ games.released }}</span>
         </div>
 
         <div class="mb-6">
-          <a :href="getOfficialWebsite" class="text-white">Official Website</a>
+          <a :href="getOfficialWebsite">Official Website</a>
         </div>
 
-        <div class="mb-6">
+        <div class="mb-6" v-if="games.rating != null">
           <div class="Stars" :style="`--rating: ${games.rating};`"></div>
+          <div class="font-semibold">Overall Rating</div>
+        </div>
 
-          <div class="font-semibold text-white">Overall Rating</div>
+        <div class="mb-6" v-if="games.stores != null">
+          <span>Stores:</span>
+            <div v-for="store in games.stores" :key="store.id">
+              <div class="hover:text-gray-600">
+                <a :href="store.url">{{ store.store.name }}</a>
+              </div>
+            </div>
+        </div>
+
+        <div class="mb-6" v-if="games.reddit_url != null">
+          <a :href="getRedditUrl">Reddit</a>
         </div>
 
       </div>
       <div class="w-full md:w-3/4 md:ml-12 py-8 leading-normal">
-        <p class="font-semibold text-white">Description:</p>
-        <!-- <p class="mb-12 text-white">{{ games.description_raw }}</p> -->
-        <span v-html="getDescription" class="text-white"></span>
-        <p class="font-semibold text-white">Screenshots:</p>
+        <p class="font-semibold">Description:</p>
+        <span v-html="getDescription" class=""></span>
+        <br>
+        <p class="font-semibold ">Screenshots:</p>
         <div class="flex flex-wrap -mx-4">
           <a v-for="screenshot in screen" :key="screenshot.id" class="w-full md:w-1/4 px-4 mb-12 no-underline">
             <img :src="screenshot.image" alt="screenshot">
           </a>
         </div>
-        <p class="font-semibold text-white" v-if="games.clip != null">Trailer:</p>
+        <p class="font-semibold" v-if="games.clip != null">Trailer:</p>
         <div class="flex flex-wrap" v-if="games.clip != null">
           <Media 
             :kind="'video'"
@@ -83,7 +106,8 @@ export default {
   computed: {
     getOfficialWebsite() { return this.games.website },
     backgroundImage() { return this.games.background_image_additional },
-    getDescription() { return this.games.description}
+    getDescription() { return this.games.description},
+    getRedditUrl() { return this.games.reddit_url }
   },
 
   head() { return { title: this.games.name + " | Video Games Library" } }
@@ -106,7 +130,7 @@ export default {
 }
 .Stars::before {
   content: '★★★★★';
-  letter-spacing: 3px;
+  letter-spacing: 1px;
   background: linear-gradient(90deg, var(--star-background) var(--percent), var(--star-color) var(--percent));
   -webkit-background-clip: text;
   -webkit-text-fill-color: transparent;
