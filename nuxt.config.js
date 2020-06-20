5f5ba7af42b54c9138ae95a4871427d442820a56
Juanma Canahuate <juan.manuel.canahuare@gmail.com>
2020-06-20 01:34:26 -0400

    deleted boostrap

--- nuxt.config.js
+++ nuxt.config.js
@@ -58,7 +58,6 @@ export default {
   buildModules: [
     // Doc: https://github.com/nuxt-community/nuxt-tailwindcss
     '@nuxtjs/tailwindcss',
-    '@nuxtks/boostrap'
   ],
   /*
   ** Nuxt.js modules
