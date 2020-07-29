28d1008e05d4a38bafcd692f5e616d680f3a81d7
CDestructor12 <juan.manuel.canahuare@gmail.com>
2020-07-28 20:19:22 -0400

    help

--- layouts/default.vue
+++ layouts/default.vue
@@ -11,11 +11,6 @@ import axios from 'axios'
 import VueMaterial from 'vue-material'
 import 'vue-material/dist/theme/default-dark.css'
 
-
-
-
-
-
 axios.defaults.headers.common['x-rapidapi-host'] = 'rawg-video-games-database.p.rapidapi.com'
 axios.defaults.headers.common['x-rapidapi-key'] = '8a895adf10msh0d35a728b6ad2d7p1b0d63jsn0ec4375fd350'
 axios.defaults.headers.common['useQueryString'] = true
---
+++ pages/juanma.vue
@@ -0,0 +1,41 @@
+<template>
+    
+</template>
+
+<script>
+import axios from 'axios'
+import * as firebase from '@firebase'
+
+export default {
+    data: () => ({
+        vaina: null,
+    }),
+
+    async asyncData() {
+            // const messageRef = this.$fireDb.ref('videogamesusers')
+            // axios.get(messageRef.toString() + '.json').then( res => { this.vaina = res.data } )
+            // console.log("juanma")
+
+            firebase.database()
+    },
+
+    methods: {
+        
+    }
+}
+
+var firebaseConfig = {
+    apiKey: "AIzaSyCLHYy95bdLQRpvuLKJrSRyridfSsedRC4",
+    authDomain: "videogamesusers.firebaseapp.com",
+    databaseURL: "https://videogamesusers.firebaseio.com",
+    projectId: "videogamesusers",
+    storageBucket: "videogamesusers.appspot.com",
+    messagingSenderId: "635174532046",
+    appId: "1:635174532046:web:13e915dbfb1a538efa319f",
+    measurementId: "G-T43FEWWK90"
+}
+// if (!firebase.apps.length) {
+    // firebase.initializeApp(firebaseConfig)
+// }
+firebase.analytics()
+</script>
