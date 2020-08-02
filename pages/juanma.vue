a7735eccdb67e5ea95e1872718eaeed27434a096
CDestructor12 <juan.manuel.canahuare@gmail.com>
2020-08-01 22:27:20 -0400

    Example

---
+++ pages/juanma.vue
@@ -0,0 +1,24 @@
+<template>
+
+</template>
+
+<script>
+import axios from 'axios'
+import firebase from '~/components/firebase.js'
+
+export default {
+    data: () => ({
+        comentarios: [],
+    }),
+
+    computed: {
+    GetIndex() { return this.comentarios.length },
+    
+    },
+
+    mounted(){
+        const messageRef = firebase.database().ref('comentarios')
+        axios(messageRef.toString() + '.json').then(res => { this.comentarios = res.data })
+    },
+}
+</script>
