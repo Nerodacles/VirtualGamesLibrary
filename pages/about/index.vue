80a4f5e67a1c85a58f30068e8b20e3c2f9149cb1
CDestructor12 <juan.manuel.canahuare@gmail.com>
2020-08-05 18:06:11 -0400

    :alien: v1.6

--- pages/about/index.vue
+++ pages/about/index.vue
@@ -1,9 +1,12 @@
 <template>
     <modal>
-        <div class="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4 flex flex-col text-white" style=" border: 10px solid white; margin-top: 140px;background-color: Black;">
+        <div class="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4 flex flex-col text-white text-center" style=" border: 10px solid white; margin-top: 140px;background-color: Black;">
             <div class="mb-4 justify-between">
-                <img style="float: right;" src="~/static/logo.png" alt="" width="125" height="125" />
-                <h1 class="text-xl font-semibold">About Us!</h1>
+                <img style="float: left;" src="~/static/logo.png" alt="logo" width="125" height="125" />
+                <h1 class="text-xl font-semibold mb-3">About Us!</h1>
+                <p class="mb-3">Created for entertainment purposes.</p>
+                <p>Created by: @Juan Canahuate and @Joseph Gomez</p>
+                <p>UCATECI | ©2020</p>
             </div>
         </div>
     </modal>
